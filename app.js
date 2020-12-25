32c3ddae2dc84ebedf892a19cabab2d1cd5f1f43
Polo-Ralphlauren <76414054+Polo-Ralphlauren@users.noreply.github.com>
2020-12-25 16:46:05 +0900

    2.5

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d"); //canvas는 'context'를 가지고 있어서 그걸로 안의 pixel들을 manipulate하는 작업 가능
-const colors = document.getElementsByClassName("jsColor");
+const colors = document.getElementsByClassName("jsColor"); //html collection(array로 바꿔줘야 함)
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const INITIAL_COLOR = "#2c2c2c";
 
 /*여기서도 width와 height 지정해줘야 함. 이유는 잘 모르겠... */
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "#2c2c2c"; //그을 선의 색
+ctx.strokeStyle = INITIAL_COLOR; //그을 선의 색
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5; //그을 선의 너비
 
 let painting = false;
+let filling = false; //filling mode면 brush로 그리는 것이 아니라 한 번에 채워야 함
 
 function stopPainting(event) {
   painting = false;
@@ -38,6 +44,28 @@ function onMouseMove(event) {
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor; //click한 거의 background color
   ctx.strokeStyle = color;
+  ctx.fillStyle = color; //canvas 한 번에 채울 사각형 만들기 위함
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height); //꽉 채운 것
+  } // 버튼을 통해 filling이 true가 되었을 떄만 채워지도록
 }
 
 if (canvas) {
@@ -45,8 +73,19 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting); // mouse click
   canvas.addEventListener("mouseup", stopPainting); //마우스를 뗐을 때
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  //range가 undefined 되어 있을 경우 예방
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  //fill button과 관련된 사항들
+  mode.addEventListener("click", handleModeClick);
+}
